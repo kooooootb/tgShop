5537fcfd7f638acb15aa1e2939175676cd3b3ba4
zavgm <zavgmaxden@mail.ru>
2022-11-07 15:40:23 +0300

    image url is absolute

--- snickersMarket/models.py
+++ snickersMarket/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
 
     color = models.CharField(max_length=100)
 
-    image = models.ImageField(upload_to='static/images/')
+    image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return f'{self.name}'
