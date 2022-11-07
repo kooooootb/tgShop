f3e85316e325dfdebed0c5bae8eafcc92e75609a
zavgm <zavgmaxden@mail.ru>
2022-11-07 15:26:21 +0300

    image url is absolute

--- tgShop/settings.py
+++ tgShop/settings.py
@@ -129,6 +129,10 @@ STATIC_ROOT = '/var/www/tgshop.hopto.org/static'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = '/var/www/tgshop.hopto.org/media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
