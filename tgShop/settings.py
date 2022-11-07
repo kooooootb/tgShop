f89b6a0bc97741ee6ac14afd4fccc3c9a2245a54
zavgm <zavgmaxden@mail.ru>
2022-11-07 18:42:31 +0300

    added trailing slash to media url

--- tgShop/settings.py
+++ tgShop/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'https://tgshop.hopto.org/media'
 
-MEDIA_ROOT = '/var/www/tgshop.hopto.org/media'
+MEDIA_ROOT = '/var/www/tgshop.hopto.org/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
