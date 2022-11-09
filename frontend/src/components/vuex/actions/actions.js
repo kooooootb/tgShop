cc9094189860a40b4818b38065060fb02b6574e3
dimakpa <dimaka02@icloud.com>
2022-11-09 17:24:46 +0300

    POST_ADD_TO_CART axios.post.2

--- frontend/src/components/vuex/actions/actions.js
+++ frontend/src/components/vuex/actions/actions.js
@@ -21,15 +21,17 @@ export default {
 
 
     POST_ADD_TO_CART({commit}, product, user_id) {
-        axios.post('https://tgshop.hopto.org/api/add_bag/?user_id=' + user_id, product)
+        axios.post('https://tgshop.hopto.org/api/add_bag/?user_id=' + user_id,{
+            body: product
+        })
             .then((user_id) => {
-                commit('GET_PRODUCTS_FROM_API_CART', user_id);
+                commit('SET_PRODUCTS_TO_CART', user_id);
         })
             .catch((error) => {
                 console.error("There was an error!", error);
             });
 
-        commit('SET_CART', product)
+
     },
     DELETE_FROM_CART({commit}, index) {
         commit('REMOVE_FROM_CART', index)
